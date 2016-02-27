56cf13ecb368141682eeaf3f0b36a187f2d24f08
Marcus T <marcust261@icloud.com>
2016-02-27 14:01:27 +0000

    Corrected broken view template references.

--- src/resources/views/filterpack.blade.php
+++ src/resources/views/filterpack.blade.php
@@ -3,8 +3,8 @@
         <div class="ibox">
             <div class="ibox-content gray-bg no-borders">
                 <h2>
-                    @include('templates.components.filterable.date-desc', ['no_tags' => true])
-                    @include('templates.components.filterable.date-nav-bar', ['size' => 'md'])
+                    @include('filterable::date-desc', ['no_tags' => true])
+                    @include('filterable::date-nav-bar', ['size' => 'md'])
                 </h2>
             </div>
         </div>
