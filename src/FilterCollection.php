ac6f7493ebf39eaed39b3e656bb8b51fc87e4ed3
Marcus T <marcust261@icloud.com>
2016-11-14 15:09:16 +0000

    These should be achieved using the functionality already available within collections; if at all.

--- src/FilterCollection.php
+++ src/FilterCollection.php
@@ -12,25 +12,6 @@ use Illuminate\Support\Collection;
 class FilterCollection extends Collection
 {
     /**
-     * @inherit
-     */
-    public function toJson($options = 0)
-    {
-        return json_encode($this->toArray(), $options);
-    }
-
-//    /**
-//     * Calculates the difference between two collections by their keys.
-//     *
-//     * @param Collection $collection
-//     * @return static
-//     */
-//    public function diffKeys(Collection $collection)
-//    {
-//        return $this->only($this->keys()->diff($collection->keys())->all());
-//    }
-
-    /**
      * Get the items in the collection whose keys are not present in the given items.
      *
      * @param mixed $items
@@ -40,29 +21,5 @@ class FilterCollection extends Collection
     public function diffKeys($items)
     {
         return new static(array_diff_key($this->items, $this->getArrayableItems($items)));
-    }
-
-    /**
-     * Returns any date filters contained within a collection of filters.
-     *
-     * @return static
-     */
-    public function dateFilters()
-    {
-        return $this->filter(function ($filter) {
-            return $filter->getGroup() === 'Dates';
-        });
-    }
-
-    /**
-     * Determines whether a collection contains a date filter.
-     *
-     * @return bool
-     */
-    public function hasDateFilter()
-    {
-        return !is_null($this->first(function ($filter) {
-            return $filter->getGroup() === 'Dates';
-        }));
     }
 }
